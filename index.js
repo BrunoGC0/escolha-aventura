d09e748e1bc2b23a28dca9b411354beeefa54812
Bruno Gonçalves <brunogcp28@gmail.com>
2024-08-27 12:21:02 -0400

    Exercicio 03 JS básico

---
+++ index.js
@@ -0,0 +1,9 @@
+const EscolhaAventura = confirm("Seja bem vindo(a),aventureiro(a)! Gostaria de pagar R$ 50,00 para passara noite na nossa hospedagem?")
+
+
+
+if(EscolhaAventura){
+    console.log("Ótimo! Nós temos as melhores camas de toda a região!")
+}else{
+    console.log("Que pena! Você parecia ser uma pessoa mais legal")
+}
